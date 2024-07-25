eba34907359cc5d2457bcea1f82329b10dc91158
unknown <aj882326@gmail.com>
2024-07-25 17:35:00 +0530

    WebPack Configuration Done

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,131 @@
-module.exports = {
-    entry: './src/index.js',
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/index.js', // Entry point for the application
     output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
+      filename: isProduction ? '[name].[contenthash].js' : '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
     },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
-        contentBase: './dist',
+      static: path.resolve(__dirname, 'dist'),
+      hot: true,
+      historyApiFallback: true,
+      port: 3000,
     },
     module: {
-        rules:[
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
             {
-                test:/\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            }
-        ]
-    }
-
-}
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                modules: {
+                  localIdentName: '[name]__[local]__[hash:base64:5]',
+                },
+              },
+            },
+            'postcss-loader',
+          ],
+          include: /\.module\.css$/,
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'postcss-loader',
+          ],
+          exclude: /\.module\.css$/,
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg|ico)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[path][name].[ext]',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(woff(2)?|eot|ttf|otf)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/',
+              },
+            },
+          ],
+        },
+        {
+            test: /\.(mp4|webm|ogg|avi)$/, // Video file extensions
+            use: [
+                {
+                loader: 'file-loader',
+                options: {
+                    name: 'videos/[name].[hash].[ext]', // Output folder and file name pattern
+                },
+                },
+            ],
+        },
+      ],
+    },
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new TerserPlugin({
+          parallel: true,
+        }),
+        new CssMinimizerPlugin(),
+      ],
+      splitChunks: {
+        chunks: 'all',
+      },
+      runtimeChunk: 'single',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        favicon: './public/favicon.ico',
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+      new MiniCssExtractPlugin({
+        filename: isProduction ? '[name].[contenthash].css' : '[name].css',
+        chunkFilename: isProduction ? '[id].[contenthash].css' : '[id].css',
+      }),
+      new Dotenv({
+        path: isProduction ? './.env.production' : './.env',
+      }),
+    ],
+  };
+};
